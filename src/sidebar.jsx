7a849e64d9c68c07e6364381bdfc8e2ad8f0ff33
hami <hamedbagheri1386.19@gmail.com>
2024-06-20 21:03:24 +0330

    PART2

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -13,30 +13,30 @@ const Sidebar = ()=>{
                 <h5 className='user_title mt-3 bg-white px-2 py-1 '>حامد باقری</h5>
                 <div className="provider mt-2"></div>
             </div>
-            <div className="sidebar_content">
+            <div className="sidebar_content ">
                 <ul className='w-100 mb-5'>
-                    <li className='nav_item w-100 mb-5 mt-2'>
+                    <li className='nav_item w-100 mb-4 mt-2'>
                         <NavLink to={'/home'} className={({isActive})=>{return isActive ? 'NavLink_hover NavLink_active' : 'NavLink_hover' }}>
                             خانه
                         </NavLink>
                      </li>
-                     <li className='nav_item w-100 mb-5'>
+                     <li className='nav_item w-100 mb-4'>
                         <NavLink to={'/movie'} className={({isActive})=>{return isActive ? 'NavLink_hover NavLink_active' : 'NavLink_hover' }}>
                             فیلم
                         </NavLink>
                      </li>
-                     <li className='nav_item w-100 mb-5'>
+                     <li className='nav_item w-100 mb-4'>
                         <NavLink to={'/series'} className={({isActive})=>{return isActive ? 'NavLink_hover NavLink_active' : 'NavLink_hover' }}>
                             سریال
                         </NavLink>
                      </li>
-                     <li className='nav_item w-100 mb-5'>
+                     <li className='nav_item w-100 mb-4'>
                         <NavLink to={'/animation'} className={({isActive})=>{return isActive ? 'NavLink_hover NavLink_active' : 'NavLink_hover' }}>
                             انیمیشن
                         </NavLink>
                      </li>
                      <li className='nav_item w-100' style={{marginTop:50}}>
-                        <div className="provider mb-5 mx-auto"></div>
+                        <div className="provider mb-4 mx-auto"></div>
                         <NavLink to={'/about'} className={({isActive})=>{return isActive ? 'NavLink_hover NavLink_active' : 'NavLink_hover' }}>
                             درباره ما
                         </NavLink>
@@ -44,8 +44,8 @@ const Sidebar = ()=>{
                 </ul>
             </div>
 
-            <div className="btn_sidebar w-100 d-flex flex-column align-items-center  justify-content-center">
-                <button className='btn btn-danger mt-4'>خروج از حساب کاربری</button>
+            <div className="btn_sidebar  w-100 d-flex flex-column align-items-center  justify-content-center">
+                <button className='btn btn-danger '>خروج از حساب کاربری</button>
             </div>
         </div>
     )
